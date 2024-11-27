ba31c797353082b3619c3c5a1efb9c48a4facb02
Talkar <privat@tommyarne.dk>
2024-11-27 20:33:25 +0100

    Added gaming layers

--- keyboards/keychron/q11/iso_encoder/keymaps/talkar/keymap.c
+++ keyboards/keychron/q11/iso_encoder/keymaps/talkar/keymap.c
@@ -18,8 +18,8 @@
 #include "print.h"
 
 enum layers{
-    MAC_BASE,
-    MAC_FN,
+    GAMING_BASE,
+    GAMING_FN,
     WIN_BASE,
     WIN_FN,
     TEST_1
@@ -36,20 +36,20 @@ enum custom_keycodes {
 #define NUM_LEDS_ON_LEFT_SIDE 43
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_92_iso(
-        KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
-        _______,  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_SPC,             KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+    [GAMING_BASE] = LAYOUT_92_iso(
+        KC_MUTE,  KC_CAPS,  KC_F1,      KC_F2,    KC_F3,   KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
+        _______,  KC_GRV,   KC_1,       KC_2,     KC_3,    KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,        KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        _______,  KC_TAB,   KC_Q,       KC_W,     KC_E,    KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
+        _______,  KC_ESC,   KC_A,       KC_S,     KC_D,    KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
+        _______,  KC_LSFT,  KC_NUBS,    KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,     KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        _______,  KC_LCTL,  MO(GAMING_FN), KC_LWIN,  KC_LALT,        KC_SPC,                        LT(GAMING_FN, KC_SPC), KC_RALT,  MO(GAMING_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
-    [MAC_FN] = LAYOUT_92_iso(
-        RGB_TOG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  RGB_TOG,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,    _______,                      _______,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
+    [GAMING_FN] = LAYOUT_92_iso(
+        RGB_TOG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  _______,
+        _______,  _______,  KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,   KC_7,     KC_8,     KC_9,     _______,  _______,    _______,  _______,            _______,
+        _______,  _______,  KC_P,     KC_O,     KC_I,     KC_U,     KC_Y,      KC_4,     KC_5,     KC_6,     _______,  _______,  _______,    _______,                      _______,
+        _______,  _______,  KC_SCLN,  KC_L,     KC_K,     KC_J,     KC_H,      KC_1,     KC_2,     KC_3,     _______,  _______,  _______,    _______,  _______,            _______,
+        _______,  _______,  _______,  KC_SLSH,  KC_DOT,   KC_COMM,  KC_M,      KC_N,     KC_0,     _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_92_iso(
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_TAB,   KC_Q,       KC_W,     KC_E,    KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
         _______,  KC_ESC,   KC_A,       KC_S,     KC_D,    KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
         _______,  KC_LSFT,  KC_NUBS,    KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  MO(WIN_FN), KC_LWIN,  KC_LALT,           MT(MOD_LSFT, KC_SPC),          LT(TEST_1, KC_SPC), KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        _______,  KC_LCTL,  MO(TEST_1), KC_LWIN,  KC_LALT,           KC_SPC,                        LT(TEST_1, KC_SPC), KC_RALT,  MO(TEST_1), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_92_iso(
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [MAC_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [GAMING_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [GAMING_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [WIN_FN]   = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
     [TEST_1]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
@@ -112,23 +112,61 @@ void set_color_split(uint8_t key_code, uint8_t r, uint8_t g, uint8_t b) {
 
 bool rgb_matrix_indicators_user(void) {
 	    switch(get_highest_layer(layer_state|default_layer_state)) {
-		    case WIN_BASE:
-                rgb_matrix_set_color_all(0,255,0);
+            case GAMING_BASE:
+                rgb_matrix_set_color_all(RGB_GREEN);
+                break;
+            case GAMING_FN:
+		        rgb_matrix_set_color_all(RGB_BLACK);
+                set_color_split(9,RGB_BLUE); // 1
+                set_color_split(10,RGB_BLUE); // 2
+                set_color_split(11,RGB_BLUE); // 3
+                set_color_split(12,RGB_BLUE); // 4
+                set_color_split(13,RGB_BLUE); // 5
+                set_color_split(14,RGB_BLUE); // 6
+                set_color_split(17,RGB_RED); // Q
+                set_color_split(18,RGB_RED); // W
+                set_color_split(19,RGB_RED); // E
+                set_color_split(20,RGB_RED); // R
+                set_color_split(21,RGB_RED); // T
+                set_color_split(24,RGB_RED); // A
+                set_color_split(25,RGB_RED); // S
+                set_color_split(26,RGB_RED); // D
+                set_color_split(27,RGB_RED); // F
+                set_color_split(28,RGB_RED); // G
+                set_color_split(32,RGB_RED); // Z
+                set_color_split(33,RGB_RED); // X
+                set_color_split(34,RGB_RED); // C
+                set_color_split(35,RGB_RED); // V
+                set_color_split(36,RGB_RED); // B
+                set_color_split(51,RGB_BLUE); // 7
+                set_color_split(52,RGB_BLUE); // 8
+                set_color_split(53,RGB_BLUE); // 9
+                set_color_split(59,RGB_BLUE); // Y
+                set_color_split(60,RGB_BLUE); // U
+                set_color_split(61,RGB_BLUE); // I
+                set_color_split(67,RGB_BLUE); // H
+                set_color_split(68,RGB_BLUE); // J
+                set_color_split(69,RGB_BLUE); // K
+                set_color_split(76,RGB_BLUE); // N
+
+                break;
+            case WIN_BASE:
+                rgb_matrix_set_color_all(RGB_CYAN);
                 break;
             case TEST_1:
-                rgb_matrix_set_color_all(255,0,0);
-                set_color_split(17,255,0,255); // Q
-                set_color_split(18,255,0,255); // W
-                set_color_split(51,0,0,255); // 7
-                set_color_split(52,0,0,255); // 8
-                set_color_split(53,0,0,255); // 9
-                set_color_split(59,0,0,255); // Y
-                set_color_split(60,0,0,255); // U
-                set_color_split(61,0,0,255); // I
-                set_color_split(67,0,0,255); // H
-                set_color_split(68,0,0,255); // J
-                set_color_split(69,0,0,255); // K
-                set_color_split(76,0,0,255); // N
+                rgb_matrix_set_color_all(RGB_MAGENTA);
+                set_color_split(17,RGB_CHARTREUSE); // Q
+                set_color_split(18,RGB_CHARTREUSE); // W
+                set_color_split(51,RGB_BLUE); // 7
+                set_color_split(52,RGB_BLUE); // 8
+                set_color_split(53,RGB_BLUE); // 9
+                set_color_split(59,RGB_BLUE); // Y
+                set_color_split(60,RGB_BLUE); // U
+                set_color_split(61,RGB_BLUE); // I
+                set_color_split(67,RGB_BLUE); // H
+                set_color_split(68,RGB_BLUE); // J
+                set_color_split(69,RGB_BLUE); // K
+                set_color_split(76,RGB_BLUE); // N
                 break;
             default: //  for any other layers, or the default layer
                 break;
