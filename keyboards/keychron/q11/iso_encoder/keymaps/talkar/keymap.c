f7af4767884f19aa5d7cc8aad6cd3bff0ac86424
Talkar <privat@tommyarne.dk>
2024-11-27 20:56:20 +0100

    Moved layer toggle to nubs button on gaming layer

--- keyboards/keychron/q11/iso_encoder/keymaps/talkar/keymap.c
+++ keyboards/keychron/q11/iso_encoder/keymaps/talkar/keymap.c
@@ -37,12 +37,12 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [GAMING_BASE] = LAYOUT_92_iso(
-        KC_MUTE,  KC_CAPS,  KC_F1,      KC_F2,    KC_F3,   KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  KC_GRV,   KC_1,       KC_2,     KC_3,    KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,        KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        _______,  KC_TAB,   KC_Q,       KC_W,     KC_E,    KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
-        _______,  KC_ESC,   KC_A,       KC_S,     KC_D,    KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,  KC_NUBS,    KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,     KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        _______,  KC_LCTL,  MO(GAMING_FN), KC_LWIN,  KC_LALT,        KC_SPC,                        LT(GAMING_FN, KC_SPC), KC_RALT,  MO(GAMING_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_MUTE,  KC_CAPS,  KC_F1,         KC_F2,    KC_F3,   KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
+        _______,  KC_GRV,   KC_1,          KC_2,     KC_3,    KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,        KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        _______,  KC_TAB,   KC_Q,          KC_W,     KC_E,    KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
+        _______,  KC_ESC,   KC_A,          KC_S,     KC_D,    KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
+        _______,  KC_LSFT,  MO(GAMING_FN), KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,     KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+        _______,  KC_LCTL,  MO(GAMING_FN), KC_LWIN,  KC_LALT, KC_SPC,              LT(GAMING_FN, KC_SPC), KC_RALT,  MO(GAMING_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [GAMING_FN] = LAYOUT_92_iso(
         RGB_TOG,  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   _______,  _______,  _______,
@@ -151,7 +151,7 @@ bool rgb_matrix_indicators_user(void) {
 
                 break;
             case WIN_BASE:
-                rgb_matrix_set_color_all(RGB_CYAN);
+                rgb_matrix_set_color_all(0,220,30);
                 break;
             case TEST_1:
                 rgb_matrix_set_color_all(RGB_MAGENTA);
