4b9d899dbf4fb6a3ce65d9f57d193e49eea0d778
Tommy Hansen <th@inmobile.com>
2025-05-27 14:55:10 +0200

    added colemak on dev

--- keyboards/keychron/q11/iso_encoder/keymaps/talkar/keymap.c
+++ keyboards/keychron/q11/iso_encoder/keymaps/talkar/keymap.c
@@ -52,12 +52,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_SLSH,  KC_DOT,   KC_COMM,  KC_M,      KC_N,                         KC_0,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,              XXXXXXX,  XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                                                    XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX),
 
-    [DEV_BASE] = LAYOUT_92_iso(
+ /*    [DEV_BASE] = LAYOUT_92_iso(
         QK_BOOT,  KC_CAPS,    KC_F1,      KC_F2,    KC_F3,   KC_F4,    KC_F5,     KC_F6,                    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   QK_BOOT,
         _______,  MO(DEV_FN), KC_1,       KC_2,     KC_3,    KC_4,     KC_5,      KC_6,                     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,     KC_Q,       KC_W,     KC_E,    KC_R,     KC_T,      KC_Y,                     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,                      KC_PGDN,
         _______,  KC_ESC,     KC_A,       KC_S,     KC_D,    KC_F,     KC_G,      KC_H,                     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,    KC_NUHS,  KC_ENT,             KC_HOME,
         _______,  KC_LSFT,    MO(DEV_FN), KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,                     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              LSFT(KC_COMM),  KC_UP,
+        _______,  KC_LCTL,    MO(DEV_FN), KC_LWIN,  KC_LALT,           KC_SPC,                                        KC_SPC, KC_RALT,  MO(DEV_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+*/
+
+   [DEV_BASE] = LAYOUT_92_iso(
+        QK_BOOT,  KC_CAPS,    KC_F1,      KC_F2,    KC_F3,   KC_F4,    KC_F5,     KC_F6,                    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   QK_BOOT,
+        _______,  MO(DEV_FN), KC_1,       KC_2,     KC_3,    KC_4,     KC_5,      KC_6,                     KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,     KC_EQL,   KC_BSPC,            KC_PGUP,
+        _______,  KC_TAB,     KC_Q,       KC_W,     KC_F,    KC_P,     KC_B,                                KC_J,     KC_L,     KC_U,     KC_Y,     LSFT(KC_COMM), KC_LBRC,    KC_RBRC,         KC_PGDN,
+        _______,  KC_ESC,     KC_A,       KC_R,     KC_S,    KC_T,     KC_G,                                KC_M,     KC_N,     KC_E,     KC_I,  KC_O,    KC_SCLN, KC_QUOT, KC_ENT,           KC_HOME,
+        _______,  KC_LSFT,    MO(DEV_FN), KC_Z,       KC_X,     KC_C,    KC_D,     KC_V,                  KC_SLSH,  KC_K,     KC_H,  KC_COMM,   KC_DOT,              LSFT(KC_COMM),  KC_UP,
         _______,  KC_LCTL,    MO(DEV_FN), KC_LWIN,  KC_LALT,           KC_SPC,                                        KC_SPC, KC_RALT,  MO(DEV_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [DEV_FN] = LAYOUT_92_iso(
